84e11b82a08160fd88b3a76cda6ad3763a12edd3
K1ndbeetle <alx-s@bk.ru>
2022-12-05 16:34:39 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,39 @@ public class Main {
             System.out.println(apartments[i]);
         }
         System.out.println();
+
+        //task №2
+        System.out.println("Задача №2");
+        int [] number = new int [3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            if (i == number.length - 1) {
+                System.out.print(number[i]);
+            } else {
+                System.out.print(number[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        double [] fractionalNumber = {1.57, 7.654, 9.986};
+        for (int i = 0; i < fractionalNumber.length; i++) {
+            if (i == fractionalNumber.length - 1) {
+                System.out.print(fractionalNumber[i]);
+            } else {
+                System.out.print(fractionalNumber[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        int [] apart = {11, 12, 13, 21, 22, 23, 31, 32, 33};
+        for (int i = 0; i < apart.length; i++) {
+            if (i == apart.length - 1) {
+                System.out.print(apart[i]);
+            } else {
+                System.out.print(apart[i] + ", ");
+            }
+        }
     }
 }
